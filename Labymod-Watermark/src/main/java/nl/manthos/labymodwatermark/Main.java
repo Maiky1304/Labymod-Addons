d38a35a5ef4f7e3c6af74c3cf043ad4ad7dac2d1
Matthijs 'Manthos' van Gent <thijs130505@gmail.com>
2021-07-21 13:20:48 +0200

    Added default project to GitHub

--- Labymod-Watermark/src/main/java/nl/manthos/labymodwatermark/Main.java
+++ Labymod-Watermark/src/main/java/nl/manthos/labymodwatermark/Main.java
@@ -2,16 +2,11 @@ package nl.manthos.labymodwatermark;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class LabymodWatermark extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
