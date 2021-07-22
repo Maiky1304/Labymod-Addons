7242edf89bf171a18ba8968ac0b7ebaa76f953ee
Matthijs 'Manthos' van Gent <thijs130505@gmail.com>
2021-07-22 15:46:51 +0200

    Default project!

---
+++ VoiceChat/src/main/java/nl/manthos/labymodvoicechat/Main.java
@@ -0,0 +1,12 @@
+package nl.manthos.labymodvoicechat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
